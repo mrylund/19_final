6fb8dc442bc8d0114b3879b657e9922ff5a04cca
Patrick <patrick.lopdrup@gmail.com>
2019-01-15 12:07:00 +0100

    lavet metoder i readfile klassen

--- src/main/java/Logic/ReadFile.java
+++ src/main/java/Logic/ReadFile.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 public class ReadFile {
 
+    private String fieldInfoPath = "txtFiles/fieldInfo.txt";
+
     public String[] readFile(String fileToBeRead) {
 
         String fileName = fileToBeRead;
@@ -18,7 +20,7 @@ public class ReadFile {
             int i = 0;
             int counter = 0;
 
-            while(reader.readLine() != null){
+            while (reader.readLine() != null) {
                 counter++;
             }
             reader = new BufferedReader(new java.io.FileReader(fileName));
@@ -26,7 +28,7 @@ public class ReadFile {
 
             lines = new String[counter];
 
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 lines[i] = line;
                 i++;
             }
@@ -41,6 +43,64 @@ public class ReadFile {
         return empty;
     }
 
+    String[] theFile = readFile(fieldInfoPath);
+
+    public String getLine(int lineNum) {
+        String[] line = readFile(fieldInfoPath);
+        return line[lineNum];
+    }
+
+    public String getFieldType(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum].split("; ")[0];
+    }
+
+    public String getFieldName(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[2];
+    }
+
+    public String getFieldPrice(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[3];
+    }
+
+    public String getFieldRent(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[4];
+    }
+
+    public String getFieldHouse1(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[5];
+    }
+
+    public String getFieldHouse2(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[6];
+    }
+
+    public String getFieldHouse3(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[7];
+    }
+
+    public String getFieldHouse4(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[8];
+    }
+
+    public String getFieldHotel(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[9];
+    }
+
+    public String getFieldBuildPrice(int lineNum) {
+        String[] allLines = readFile(fieldInfoPath);
+        return allLines[lineNum-1].split("; ")[10];
+    }
+
+
     public static void main(String[] args){
         ReadFile reader = new ReadFile();
         String[] readFilePrint = reader.readFile("txtFiles/fieldInfo.txt");
@@ -53,6 +113,6 @@ public class ReadFile {
 
         System.out.println(readFilePrint[39].split("; ")[5]);
 
-
+        System.out.println(reader.getFieldName(40));
     }
 }
