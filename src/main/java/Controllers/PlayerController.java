f29c46d09ac2e484cc82a41098893973a729c959
Martin Rylund <SnowredWolf@gmail.com>
2019-01-15 19:29:03 +0100

    Tilføjet en PlayerController til at manage spillerne i spillet

---
+++ src/main/java/Controllers/PlayerController.java
@@ -0,0 +1,25 @@
+package Controllers;
+import Entities.Player;
+import Entities.PlayerList;
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+
+public class PlayerController {
+    private PlayerList players = new PlayerList();
+
+    public Player[] createPlayers(int antalSpillere) {
+        return players.addPlayers(antalSpillere);
+    }
+
+    public GUI_Car[] getPlayerCars() {
+        return players.getCars();
+    }
+
+    public GUI_Player[] getPlayersGUI() {
+        GUI_Player[] guiPlayers = new GUI_Player[players.getPlayers().length];
+        for (int i = 0; i < players.getPlayers().length ; i++) {
+            guiPlayers[i] = players.getPlayerGUI(i);
+        }
+        return guiPlayers;
+    }
+}
