f886154f8846c8265c35036919ede494c2337453
Martin Rylund <SnowredWolf@gmail.com>
2019-01-15 19:25:52 +0100

    Tilføjet player class med mulighed for at sætte farve

---
+++ src/main/java/Entities/Player.java
@@ -0,0 +1,30 @@
+package Entities;
+
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+
+import java.awt.*;
+
+public class Player {
+    private String name;
+    private int balance;
+    private int pos;
+    private GUI_Car car;
+    private GUI_Player player;
+
+    public Player(Color color) {
+        name = "";
+        balance = 30000;
+        pos = 0;
+        car = new GUI_Car(color, Color.BLACK, GUI_Car.Type.CAR, GUI_Car.Pattern.FILL);
+        player = new GUI_Player(name, balance, car);
+    }
+
+    public GUI_Player getPlayer() {
+        return player;
+    }
+
+    public GUI_Car getCar() {
+        return car;
+    }
+}
