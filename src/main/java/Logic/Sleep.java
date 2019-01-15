003f3c52da83d58d343e9acd11788faa77f7184b
Martin Rylund <SnowredWolf@gmail.com>
2019-01-15 21:56:08 +0100

    Tilføjet en Sleep Class

---
+++ src/main/java/Logic/Sleep.java
@@ -0,0 +1,17 @@
+package Logic;
+
+public class Sleep {
+    public static void sleep() {
+        sleep(1200);
+    }
+
+    public static void sleep(int n) {
+        long t0 = System.currentTimeMillis();
+
+        long t1;
+        do {
+            t1 = System.currentTimeMillis();
+        } while(t1 - t0 < (long)n);
+
+    }
+}
