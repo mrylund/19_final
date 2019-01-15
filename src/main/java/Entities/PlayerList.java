2aef4b92abece09282aea0982895fd9f29999e23
Martin Rylund <SnowredWolf@gmail.com>
2019-01-15 19:26:46 +0100

    Tilføjet PlayerList class med mulighed for at tilføje alle spillere med unikke farver

---
+++ src/main/java/Entities/PlayerList.java
@@ -0,0 +1,44 @@
+package Entities;
+
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+
+import java.awt.*;
+
+public class PlayerList {
+    private Player[] players;
+    public Player[] addPlayers(int antalSpillere) {
+        Color color;
+        players = new Player[antalSpillere];
+        for(int i = 0; i < antalSpillere; i++) {
+            switch (i) {
+                case 0: color = new Color(255, 255, 255); break;
+                case 1: color = new Color(0, 0,0); break;
+                case 2: color = new Color(255, 0, 0); break;
+                case 3: color = new Color(0, 255, 0); break;
+                case 4: color = new Color(0, 0, 255); break;
+                case 5: color = new Color(255, 255, 0); break;
+                default: color = new Color(255, 255, 255); break;
+            }
+            players[i] = new Player(color);
+        }
+        return players;
+    }
+
+    public GUI_Player getPlayerGUI(int number) {
+        return players[number].getPlayer();
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public GUI_Car[] getCars() {
+        GUI_Car[] cars = new GUI_Car[players.length];
+        for(int i = 0; i < players.length; i++) {
+            cars[i] = players[i].getCar();
+        }
+
+        return cars;
+    }
+}
