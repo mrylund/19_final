bf64c35e3da80d765664670694245537caf4de45
Martin Rylund <SnowredWolf@gmail.com>
2019-01-15 22:44:27 +0100

    Tilføjet metode til at sætte bil position for spiller samt flytte en bil.

--- src/main/java/Controllers/BoardController.java
+++ src/main/java/Controllers/BoardController.java
@@ -4,6 +4,8 @@ import Entities.Player;
 import Gameboard.Gameboard;
 import gui_fields.GUI_Player;
 
+import static Logic.Sleep.sleep;
+
 public class BoardController {
     private Gameboard board = new Gameboard();
     public Gameboard createBoard() {
@@ -20,5 +22,20 @@ public class BoardController {
 
     public void setDice(int dice1, int dice2) {
         board.getBoard().setDice(dice1, dice2);
+    }
+
+    public void setCarpos(GUI_Player player, int prevpos, int pos) {
+        board.getBoard().getFields()[pos].setCar(player, true);
+    }
+
+    public void moveCar(GUI_Player player, int prevpos, int newpos) {
+        int movepos;
+        while(prevpos < newpos) {
+            movepos = prevpos + 1;
+            board.getBoard().getFields()[prevpos].setCar(player, false);
+            board.getBoard().getFields()[movepos].setCar(player, true);
+            prevpos++;
+            sleep(300);
+        }
     }
 }
