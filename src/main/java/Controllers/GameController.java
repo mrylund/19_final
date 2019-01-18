d5a32f25794c0295cc25aa51cc23d1a887223eda
Athusan <athusan_kuga@hotmail.com>
2019-01-18 16:03:12 +0100

    Case 8 i switch til chancekort ændret

--- src/main/java/Controllers/GameController.java
+++ src/main/java/Controllers/GameController.java
@@ -132,7 +132,6 @@ public class GameController {
         } else if (fieldType == 6) {
             doTaxField();
 
-
         } else if (fieldType == 7) {
             doParkingField();
 
@@ -161,13 +160,18 @@ public class GameController {
         }
     }
 
-
+    //@SuppressWarnings("Duplicates")
     private void doChanceField(int player, int prevPos, int fieldNumber) {
         chancecontroller.drawCard();
         int[] values = chancecontroller.getCardValues();
+        //felter for de 4 rederier. 0 bruges til beregning og er IKKE et rigtigt felt.
+        int[] rederier = {0,6,16,26,36};
+        int closestRederi = 0;
+
         String chanceCardText = chancecontroller.getCardText();
         boardController.displayChanceCard(chanceCardText);
         input.showMessage("Spiller: " + playerController.getPlayerGUI(player).getName() + "\nDu har trukket et chancekort.");
+
         switch(values[0]){
             case 1: //Spillers balance ændres afhængig af hvad der står på kortet.
                 playerController.getPlayer(player).addBalance(values[1]);
@@ -180,6 +184,7 @@ public class GameController {
                     playerController.setPlayerPos(player,values[1]);
                 }
                 boardController.setCarpos(playerController.getPlayerGUI(player),fieldNumber,values[1]);
+                doFieldAction(player,prevPos,fieldNumber);
                 break;
 
             case 3: //Spiller trækker jailCard, som kan bruges til at komme ud af fængsel
@@ -187,20 +192,14 @@ public class GameController {
                 break;
 
             case 4: //Gå i fængsel chancekort
+                input.showMessage("Spiller: " + playerController.getPlayerGUI(player).getName() + "\nDu bliver sendt i fængsel, da du har trukket et fængsels-chancekort!");
                 boardController.setCarpos(playerController.getPlayerGUI(player),prevPos,values[1]);
-                if(playerController.getPlayer(player).hasJailcard()){
-                    playerController.getPlayer(player).setJailed(false);
-                }else {
-                    playerController.getPlayer(player).setJailed(true);
-                }
+                playerController.setPlayerPos(player,prevPos,false);
                 break;
             case 5:
                 break;
             case 6: // ryk til nærmeste rederi? og betal 2 * leje til ejeren af feltet
                 int playerPos = playerController.getPlayerPos(player);
-                //felter for de 4 rederier. 0 bruges til beregning og er IKKE et rigtigt felt.
-                int[] rederier = {0,6,16,26,36};
-                int closestRederi = 0;
                 //finder det tætteste rederi
                 if(playerPos >= rederier[4]) {
                     closestRederi = rederier[1];
@@ -231,14 +230,45 @@ public class GameController {
                     doPurchasableField(player, playerController.getPlayerPos(player)+1);
                 }
                 break;
+
             case 7: // matador legat på 40.000 hvis formuen af spiller (d.v.s. deres kontante penge + skøder + bygninger) ikke overstiger kr. 15.000
                 break;
+
             case 8: // Tag med den nærmeste færge - flyt brikken frem, og hvis de passerer “Start” indkassér da kr. 4.000.
+                // boardController.moveCar(playerController.getPlayer(curPlayer),curPos,)
+                //prevPos = playerController.getPlayerPos(player);
+                //finder det tætteste rederi
+                if(prevPos >= rederier[4]) {
+                    closestRederi = rederier[1];
+                } else {
+                    for (int i = 0; i < rederier.length-1; i++) {
+                        if(prevPos > rederier[i] && prevPos < rederier[i+1]) {
+                            closestRederi = Math.max(rederier[i], rederier[i+1]);
+                            break;
+                        }
+                    }
+                }
+                if(prevPos >= rederier[4]) {
+                    playerController.setPlayerPos(player, closestRederi, true);
+                } else {
+                    playerController.setPlayerPos(player, closestRederi);
+                }
+                boardController.setCarpos(playerController.getPlayerGUI(player), fieldNumber, closestRederi);
+                input.showMessage("Du tager nu færgen hen til næste rederi! Hvis feltet du lander på er ejet af en anden spiller, så skal der betales leje!");
+
+                playerController.setPlayerPos(player,closestRederi+10,true);
+                boardController.setCarpos(playerController.getPlayerGUI(player),prevPos,closestRederi+10);
+                int newPos = closestRederi+10;
+
+                doPurchasableField(player,newPos);
+
                 break;
+
             case 9: // Ryk tre felter frem.
                 boardController.moveCar(playerController.getPlayerGUI(player),fieldNumber,3);
                 playerController.movePlayer(player,fieldNumber,3);
                 break;
+
             case 10: // 200 kr fra alle spillere til curPlayer
 
                 playerController.getPlayer(player).addBalance(numberOfPlayers * 200);
@@ -251,9 +281,7 @@ public class GameController {
 
             default:
 
-
         }
-
     }
 
     private void doGoJailField(int player, int fieldNumber) {
